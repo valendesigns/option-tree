66ddc4e5e7f523b245f01ca18ee30c1b31f2f9e3
Ruan Mer <ruanrm@gmail.com>
2012-03-20 21:31:20 -0300

    Added ability to include more than one post type
    Its work for Custom Post and Custom Posts option. Ex:
    post-type-1,post-type-2

--- functions/admin/custom-post.php
+++ functions/admin/custom-post.php
@@ -21,18 +21,22 @@ function option_tree_custom_post( $value, $settings, $int )
         <div class="select_wrapper">
           <select name="<?php echo $value->item_id; ?>" id="<?php echo $value->item_id; ?>" class="select">
           <?php
-       		$posts = &get_posts( array( 'post_type' => trim($value->item_options), 'numberposts' => -1, 'orderby' => 'title', 'order' => 'ASC' ) );
-       		if ( $posts )
-       		{
+          $posts = &get_posts( array( 'post_type' => explode(',', $value->item_options), 'numberposts' => -1, 'orderby' => 'title', 'order' => 'ASC' ) );
+          if ( $posts )
+          {
             echo '<option value="">-- Choose One --</option>';
             foreach ( $posts as $post ) 
             {
               $selected = '';
-    	        if ( isset( $settings[$value->item_id] ) && $settings[$value->item_id] == $post->ID ) 
-    	        { 
+              if ( isset( $settings[$value->item_id] ) && $settings[$value->item_id] == $post->ID ) 
+              { 
                 $selected = ' selected="selected"'; 
               }
-            	echo '<option value="'.$post->ID.'"'.$selected.'>'.$post->post_title.'</option>';
+          
+              $postTypeObj = get_post_type_object( $post->post_type );
+              $postTypeName = $postTypeObj->labels->singular_name;
+
+              echo '<option value="'.$post->ID.'"'.$selected.'>'.$post->post_title.' ('.$postTypeName.')</option>';
             }
           } 
           else 
@@ -72,31 +76,35 @@ function option_tree_custom_posts( $value, $settings, $int )
       <div class="element">
         <?php
         // check for settings item value 
-	      if ( isset( $settings[$value->item_id] ) ) {
+        if ( isset( $settings[$value->item_id] ) ) {
           $ch_values = (array) $settings[$value->item_id];
         } else {
           $ch_values = array();
         }
         // loop through tags
-	      $posts = &get_posts( array( 'post_type' => $value->item_options, 'numberposts' => -1, 'orderby' => 'title', 'order' => 'ASC' ) );
-       	if ( $posts )
-       	{
-       	  $count = 0;
-  	      foreach ( $posts as $post ) 
-  	      {
+        $posts = &get_posts( array( 'post_type' => explode(',', $value->item_options), 'numberposts' => -1, 'orderby' => 'title', 'order' => 'ASC' ) );
+        if ( $posts )
+        {
+          $count = 0;
+          foreach ( $posts as $post ) 
+          {
             $checked = '';
-  	        if ( in_array( $post->ID, $ch_values ) ) 
-  	        { 
+            if ( in_array( $post->ID, $ch_values ) ) 
+            { 
               $checked = ' checked="checked"'; 
             }
-  	        echo '<div class="input_wrap"><input name="'.$value->item_id.'['.$count.']" id="'.$value->item_id.'_'.$count.'" type="checkbox" value="'.$post->ID.'"'.$checked.' /><label for="'.$value->item_id.'_'.$count.'">'.$post->post_title.'</label></div>';
-  	        $count++;
-       		}
-       	}
-       	else
-       	{
-       	  echo '<p>No Custom Posts Available</p>';
-       	}
+                      
+            $postTypeObj = get_post_type_object( $post->post_type );
+            $postTypeName = $postTypeObj->labels->singular_name;
+
+            echo '<div class="input_wrap"><input name="'.$value->item_id.'['.$count.']" id="'.$value->item_id.'_'.$count.'" type="checkbox" value="'.$post->ID.'"'.$checked.' /><label for="'.$value->item_id.'_'.$count.'">'.$post->post_title.' ('.$postTypeName.')</label></div>';
+            $count++;
+          }
+        }
+        else
+        {
+          echo '<p>No Custom Posts Available</p>';
+        }
         ?>
       </div>
       <div class="description">
