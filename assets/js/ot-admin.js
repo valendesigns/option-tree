1f6d3c7c783a18d56f617043274cb54f39741ed9
Derek Herman <derek@valendesigns.com>
2012-07-12 21:18:46 -0700

    fix layout select JS

--- assets/js/ot-admin.js
+++ assets/js/ot-admin.js
@@ -160,6 +160,7 @@
           $('#option-tree-options-layouts-form').submit();
         } else {
           var active = $('#the_current_layout').attr('value');
+          $('#option-tree-options-layouts-form select option[value="' + active + '"]').attr({'selected':'selected'});
           $('#option-tree-options-layouts-form select').prev('span').replaceWith('<span>' + active + '</span>');
         }
       });
