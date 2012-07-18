e52a433026a219e52a5ccb0ca6bab593e53920c9
Derek Herman <derek@valendesigns.com>
2012-07-18 10:39:59 -0700

    fix import from table data

--- includes/ot-functions-admin.php
+++ includes/ot-functions-admin.php
@@ -256,7 +256,7 @@ if ( ! function_exists( 'ot_default_settings' ) ) {
       $settings_count = 0;
       $settings = array();
       
-      if ( mysql_num_rows( mysql_query( "SHOW TABLES LIKE '{$table_prefix}option_tree'" ) ) == 1 && $old_settings = $wpdb->get_results( "SELECT * FROM {$table_prefix}option_tree" ) ) {
+      if ( mysql_num_rows( mysql_query( "SHOW TABLES LIKE '{$table_prefix}option_tree'" ) ) == 1 && $old_settings = $wpdb->get_results( "SELECT * FROM {$table_prefix}option_tree ORDER BY item_sort ASC" ) ) {
         
         foreach ( $old_settings as $setting ) {
           
