fe659c72835ee28aeb36fd3b1f22873d01f9bf5d
Derek Herman <derek@valendesigns.com>
2014-02-05 14:09:48 -0800

    version bump

--- ot-loader.php
+++ ot-loader.php
@@ -3,7 +3,7 @@
  * Plugin Name: OptionTree
  * Plugin URI:  http://wp.envato.com
  * Description: Theme Options UI Builder for WordPress. A simple way to create & save Theme Options and Meta Boxes for free or premium themes.
- * Version:     2.2.3
+ * Version:     2.3.0
  * Author:      Derek Herman
  * Author URI:  http://valendesigns.com
  * License:     GPLv3
@@ -142,7 +142,7 @@ if ( ! class_exists( 'OT_Loader' ) ) {
       /**
        * Current Version number.
        */
-      define( 'OT_VERSION', '2.2.3' );
+      define( 'OT_VERSION', '2.3.0' );
       
       /**
        * For developers: Allow Unfiltered HTML in all the textareas.
