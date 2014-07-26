bac27b51f694a4d863df38085899b26e88c71fd1
Clifton Griffin <clifgriffin@gmail.com>
2014-07-26 14:27:08 -0400

    Check for post_title instead of post_name in ot_get_media_post_ID() as post slug is not guaranteed.

--- includes/ot-functions-admin.php
+++ includes/ot-functions-admin.php
@@ -692,7 +692,7 @@ if ( ! function_exists( 'ot_get_media_post_ID' ) ) {
   function ot_get_media_post_ID() {
     global $wpdb;
     
-    return $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE `post_name` = 'media' AND `post_type` = 'option-tree' AND `post_status` = 'private'" );
+    return $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE `post_title` = 'Media' AND `post_type` = 'option-tree' AND `post_status` = 'private'" );
     
   }
 
