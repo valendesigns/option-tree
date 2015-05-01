b336cb9801316056421791e5404f9f810b826289
doitmax <dennis.osterkamp@doitmax.de>
2015-05-02 00:09:07 +0200

    Parse error fix

--- includes/ot-meta-box-api.php
+++ includes/ot-meta-box-api.php
@@ -11,10 +11,10 @@
 if ( ! class_exists( 'OT_Meta_Box' ) ) {
 
   class OT_Meta_Box {
-  
+
     /* variable to store the meta box array */
     private $meta_box;
-  
+
     /**
      * PHP5 constructor method.
      *
@@ -46,7 +46,7 @@ if ( ! class_exists( 'OT_Meta_Box' ) ) {
       add_action( 'save_post', array( $this, 'save_meta_box' ), 1, 2 );
 
     }
-    
+
     /**
      * Adds meta box to any post type
      *
@@ -62,7 +62,7 @@ if ( ! class_exists( 'OT_Meta_Box' ) ) {
         add_meta_box( $this->meta_box['id'], $this->meta_box['title'], array( $this, 'build_meta_box' ), $page, $this->meta_box['context'], $this->meta_box['priority'], $this->meta_box['fields'] );
       }
     }
-    
+
     /**
      * Meta box view
      *
@@ -77,21 +77,21 @@ if ( ! class_exists( 'OT_Meta_Box' ) ) {
 
         /* Use nonce for verification */
         echo '<input type="hidden" name="' . $this->meta_box['id'] . '_nonce" value="' . wp_create_nonce( $this->meta_box['id'] ) . '" />';
-        
+
         /* meta box description */
         echo isset( $this->meta_box['desc'] ) && ! empty( $this->meta_box['desc'] ) ? '<div class="description" style="padding-top:10px;">' . htmlspecialchars_decode( $this->meta_box['desc'] ) . '</div>' : '';
-      
+
         /* loop through meta box fields */
         foreach ( $this->meta_box['fields'] as $field ) {
-        
+
           /* get current post meta data */
           $field_value = get_post_meta( $post->ID, $field['id'], true );
-          
+
           /* set standard value */
-          if ( isset( $field['std'] ) ) {  
+          if ( isset( $field['std'] ) ) {
             $field_value = ot_filter_std_value( $field_value, $field['std'] );
           }
-          
+
           /* build the arguments array */
           $_args = apply_filters( 'ot_metabox_arguments', array(
             'type'              => $field['type'],
@@ -112,68 +112,68 @@ if ( ! class_exists( 'OT_Meta_Box' ) ) {
             'field_settings'    => isset( $field['settings'] ) && ! empty( $field['settings'] ) ? $field['settings'] : array(),
             'post_id'           => $post->ID,
             'meta'              => true
-          );
-          
+          ) );
+
           $conditions = '';
-          
+
           /* setup the conditions */
           if ( isset( $field['condition'] ) && ! empty( $field['condition'] ) ) {
-  
+
             $conditions = ' data-condition="' . $field['condition'] . '"';
             $conditions.= isset( $field['operator'] ) && in_array( $field['operator'], array( 'and', 'AND', 'or', 'OR' ) ) ? ' data-operator="' . $field['operator'] . '"' : '';
-  
+
           }
-          
+
           /* only allow simple textarea due to DOM issues with wp_editor() */
           if ( apply_filters( 'ot_override_forced_textarea_simple', false, $field['id'] ) == false && $_args['type'] == 'textarea' )
             $_args['type'] = 'textarea-simple';
 
           // Build the setting CSS class
           if ( ! empty( $_args['field_class'] ) ) {
-            
+
             $classes = explode( ' ', $_args['field_class'] );
 
             foreach( $classes as $key => $value ) {
-            
+
               $classes[$key] = $value . '-wrap';
-              
+
             }
 
             $class = 'format-settings ' . implode( ' ', $classes );
-            
+
           } else {
-          
+
             $class = 'format-settings';
-            
+
           }
-          
+
           /* option label */
           echo '<div id="setting_' . $field['id'] . '" class="' . $class . '"' . $conditions . '>';
-            
+
             echo '<div class="format-setting-wrap">';
-            
+
               /* don't show title with textblocks */
               if ( $_args['type'] != 'textblock' && ! empty( $_args['field_label'] ) ) {
                 echo '<div class="format-setting-label">';
                   echo '<label for="' . $field['id'] . '" class="label">' . $_args['field_label'] . '</label>';
                 echo '</div>';
               }
-      
+
               /* get the option HTML */
               echo ot_display_by_type( $_args );
-              
+
             echo '</div>';
-            
+
           echo '</div>';
-          
+
         }
 
         echo '<div class="clear"></div>';
-      
+
       echo '</div>';
 
     }
-    
+
     /**
      * Saves the meta box values
      *
@@ -188,11 +188,11 @@ if ( ! class_exists( 'OT_Meta_Box' ) ) {
       /* don't save if $_POST is empty */
       if ( empty( $_POST ) || ( isset( $_POST['vc_inline'] ) && $_POST['vc_inline'] == true ) )
         return $post_id;
-      
+
       /* don't save during quick edit */
       if ( $pagenow == 'admin-ajax.php' )
         return $post_id;
-        
+
       /* don't save during autosave */
       if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
         return $post_id;
@@ -200,11 +200,11 @@ if ( ! class_exists( 'OT_Meta_Box' ) ) {
       /* don't save if viewing a revision */
       if ( $post_object->post_type == 'revision' || $pagenow == 'revision.php' )
         return $post_id;
-  
+
       /* verify nonce */
       if ( isset( $_POST[ $this->meta_box['id'] . '_nonce'] ) && ! wp_verify_nonce( $_POST[ $this->meta_box['id'] . '_nonce'], $this->meta_box['id'] ) )
         return $post_id;
-    
+
       /* check permissions */
       if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
         if ( ! current_user_can( 'edit_page', $post_id ) )
@@ -213,18 +213,18 @@ if ( ! class_exists( 'OT_Meta_Box' ) ) {
         if ( ! current_user_can( 'edit_post', $post_id ) )
           return $post_id;
       }
-      
+
       foreach ( $this->meta_box['fields'] as $field ) {
-        
+
         $old = get_post_meta( $post_id, $field['id'], true );
         $new = '';
-        
+
         /* there is data to validate */
         if ( isset( $_POST[$field['id']] ) ) {
-        
+
           /* slider and list item */
           if ( in_array( $field['type'], array( 'list-item', 'slider' ) ) ) {
-              
+
             /* required title setting */
             $required_setting = array(
               array(
@@ -239,101 +239,101 @@ if ( ! class_exists( 'OT_Meta_Box' ) ) {
                 'choices'   => array()
               )
             );
-            
+
             /* get the settings array */
             $settings = isset( $_POST[$field['id'] . '_settings_array'] ) ? unserialize( ot_decode( $_POST[$field['id'] . '_settings_array'] ) ) : array();
-            
+
             /* settings are empty for some odd ass reason get the defaults */
             if ( empty( $settings ) ) {
-              $settings = 'slider' == $field['type'] ? 
-              ot_slider_settings( $field['id'] ) : 
+              $settings = 'slider' == $field['type'] ?
+              ot_slider_settings( $field['id'] ) :
               ot_list_item_settings( $field['id'] );
             }
-            
+
             /* merge the two settings array */
             $settings = array_merge( $required_setting, $settings );
-            
+
             foreach( $_POST[$field['id']] as $k => $setting_array ) {
-  
+
               foreach( $settings as $sub_setting ) {
-                
+
                 /* verify sub setting has a type & value */
                 if ( isset( $sub_setting['type'] ) && isset( $_POST[$field['id']][$k][$sub_setting['id']] ) ) {
-                  
+
                   $_POST[$field['id']][$k][$sub_setting['id']] = ot_validate_setting( $_POST[$field['id']][$k][$sub_setting['id']], $sub_setting['type'], $sub_setting['id'] );
-                  
+
                 }
-                
+
               }
-            
+
             }
-            
+
             /* set up new data with validated data */
             $new = $_POST[$field['id']];
-          
+
           } else if ( $field['type'] == 'social-links' ) {
-            
+
             /* get the settings array */
             $settings = isset( $_POST[$field['id'] . '_settings_array'] ) ? unserialize( ot_decode( $_POST[$field['id'] . '_settings_array'] ) ) : array();
-            
+
             /* settings are empty get the defaults */
             if ( empty( $settings ) ) {
               $settings = ot_social_links_settings( $field['id'] );
             }
-            
+
             foreach( $_POST[$field['id']] as $k => $setting_array ) {
-  
+
               foreach( $settings as $sub_setting ) {
-                
+
                 /* verify sub setting has a type & value */
                 if ( isset( $sub_setting['type'] ) && isset( $_POST[$field['id']][$k][$sub_setting['id']] ) ) {
-                  
+
                   $_POST[$field['id']][$k][$sub_setting['id']] = ot_validate_setting( $_POST[$field['id']][$k][$sub_setting['id']], $sub_setting['type'], $sub_setting['id'] );
-                  
+
                 }
-                
+
               }
-            
+
             }
-            
+
             /* set up new data with validated data */
             $new = $_POST[$field['id']];
 
           } else {
-            
+
             /* run through validattion */
             $new = ot_validate_setting( $_POST[$field['id']], $field['type'], $field['id'] );
-            
+
           }
-          
+
           /* insert CSS */
           if ( $field['type'] == 'css' ) {
-            
+
             /* insert CSS into dynamic.css */
             if ( '' !== $new ) {
-              
+
               ot_insert_css_with_markers( $field['id'], $new, true );
-            
+
             /* remove old CSS from dynamic.css */
             } else {
-            
+
               ot_remove_old_css( $field['id'] );
-              
+
             }
-          
+
           }
-        
+
         }
-        
+
         if ( isset( $new ) && $new !== $old ) {
           update_post_meta( $post_id, $field['id'], $new );
         } else if ( '' == $new && $old ) {
           delete_post_meta( $post_id, $field['id'], $old );
         }
       }
-  
+
     }
-  
+
   }
 
 }
@@ -354,7 +354,7 @@ if ( ! function_exists( 'ot_register_meta_box' ) ) {
   function ot_register_meta_box( $args ) {
     if ( ! $args )
       return;
-      
+
     $ot_meta_box = new OT_Meta_Box( $args );
   }
 
