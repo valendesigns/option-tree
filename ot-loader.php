1f65d8000e608a3e36cebb5a4071a88fde809907
Derek Herman <derek@valendesigns.com>
2016-02-09 22:25:31 -0800

    Bump version

--- ot-loader.php
+++ ot-loader.php
@@ -3,7 +3,7 @@
  * Plugin Name: OptionTree
  * Plugin URI:  https://github.com/valendesigns/option-tree/
  * Description: Theme Options UI Builder for WordPress. A simple way to create & save Theme Options and Meta Boxes for free or premium themes.
- * Version:     2.5.5
+ * Version:     2.6.0
  * Author:      Derek Herman
  * Author URI:  http://valendesigns.com
  * License:     GPLv3
