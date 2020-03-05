3f6933ffa2308a5c128016b9cadb4d5314d73efc
Landon Otis <landon@7bidaho.com>
2020-03-04 22:35:24 -0800

    Added ability to skip iteration for empty section to prevent add_settings_section.

--- includes/class-ot-settings.php
+++ includes/class-ot-settings.php
@@ -390,6 +390,10 @@ if ( ! class_exists( 'OT_Settings' ) ) {
 					// Loop through page sections.
 					foreach ( (array) $this->get_sections( $page ) as $section ) {
 
+						if ( empty( $section ) ) {
+							continue;
+						}
+
 						// Add each section.
 						add_settings_section(
 							$section['id'],
